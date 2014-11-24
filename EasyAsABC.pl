d3b41b06bc5d199778b649861f04f5f2e6d0f731
Jorge Lima <jor.giinho12@gmail.com>
2014-11-24 19:23:46 +0000

    #1 - Board implemented, Restrictions are working limitedly.
    	 //TODO
    	 Implement restricions for side letters.
    	 Generate random playable board,
    	 Generate NxN playable board.

---
+++ EasyAsABC.pl
@@ -0,0 +1,140 @@
+:- use_module(library(lists)).
+:- use_module(library(random)).
+:- use_module(library(clpfd)).
+
+
+%%DEFINITIONS
+
+emptyBoard([[-1,-1,-1,-1],
+[-1,-1,-1,-1],
+[-1,-1,-1,-1],
+[-1,-1,-1,-1]]).
+
+emptyBoard([[-1,-1,-1,-1],
+[-1,-1,-1,-1],
+[-1,-1,-1,-1],
+[-1,-1,-1,-1]]).
+
+testBoard([[1,0,0,0],
+[0,3,0,0],
+[0,0,0,0],
+[0,4,0,0]]).
+
+
+
+testLineRestrictions1([-1,2,-1,3]).
+testLineRestrictions2([1,-1,-1,-1]).
+testLineRestrictions3([2,-1,3,-1]).
+testLineRestrictions4([-1,-1,-1,1]).
+
+
+
+convertNum(' ', -1).                   
+convertNum('X', 0).
+convertNum('A',1).
+convertNum('B',2).
+convertNum('C',3).
+convertNum('D',4).
+convertNum('E',5).
+convertNum('F',6).
+convertNum('G',7).
+convertNum('H',8).
+convertNum('I',9).
+convertNum('J',10). 
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%  Print Board and Scenario Predicates
+printLine([]).
+
+printLine([P1|Resto]):-      
+        convertNum(Val, P1),
+        print(' '),
+        print('|'),
+        print(Val),
+        printLine(Resto).
+
+printLineX([]).
+
+printLineX([P1|Resto]):-      
+        convertNum(Val, P1),
+        print(' '),
+        print(Val),
+        print(' '),
+        printLineX(Resto).
+
+printBoard([],5).
+printBoard([L1|Resto],I) :- 
+        testLineRestrictions4(Line3),
+        nth1(I, Line3, Elem),  
+        convertNum(Val, Elem),
+        print(Val),
+        printLine(L1),        
+        print(' | '),
+        testLineRestrictions2(Line2),
+        nth1(I, Line2, Elem1),
+        convertNum(Val1, Elem1),  
+        print(Val1),
+        nl,
+        I2 is I+1,
+        printBoard(Resto, I2).
+
+printLineRestrictions(Line):-
+       printLine(Line).
+  
+
+printInitialBoard:-
+        emptyBoard(Cenas),
+        nl,
+        printScenario(Cenas).
+
+printScenario(Board):- 
+        testLineRestrictions1(Line1),
+        print('  '),
+        printLineX(Line1),
+        nl,
+        printBoard(Board, 1),
+        testLineRestrictions3(Line4),
+        print('  '),
+        printLineX(Line4),nl. 
+
+fazCoisas(Board):-
+        Board = [A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15,A16],
+        domain(Board,0, 3),
+        all_distinct([A1,A2,A3,A4]),
+        all_distinct([A5,A6,A7,A8]),
+        all_distinct([A9, A10, A11, A12]),
+        all_distinct([A13, A14, A15, A16]),
+        
+        all_distinct([A1,A5,A9,A13]),
+        all_distinct([A2,A6,A10,A14]),
+        all_distinct([A3, A7, A11, A15]),
+        all_distinct([A4, A8, A12, A16]),
+        
+        labeling([], Board).
+
+createBoard([], _, BoardOut, BoardOut, 1, 5).
+
+createBoard(Rest, Accum, Line, BoardOut, 5, I2):-
+        append(Line, [Accum], Accccum),
+        IY is I2+1,
+        createBoard(Rest, [], Accccum, BoardOut, 1 , IY).
+
+createBoard([P|Rest], Accum, Line, BoardOut, I, I2):-
+        I < 6,
+        append(Accum, [P], Result),
+        IX is I+1,
+        createBoard(Rest, Result, Line, BoardOut, IX, I2).             
+
+
+printCoisas:-
+        fazCoisas(Board),
+        createBoard(Board, [], [], Scene, 1, 1),
+        printScenario(Scene).
+        
+             
+        
+
+
