92f9166fd85a29d20b975050306c2c50f020ef65
Jorge Lima <jor.giinho12@gmail.com>
2014-12-03 18:12:51 +0000

    #11 - Constrains for corner sides implemented, missing verifications and has errors. //Before testing new corners verifications

--- EasyAsABC.pl
+++ EasyAsABC.pl
@@ -37,20 +37,61 @@ convertNum('W',23).
 convertNum('Y',24).
 convertNum('Z',25).
 
-emptyBoard([[-1,-1,-1,-1],
+emptyBoard4x4([[-1,-1,-1,-1],
 [-1,-1,-1,-1],
 [-1,-1,-1,-1],
 [-1,-1,-1,-1]]).
+
+emptyBoard5x5([[-1,-1,-1,-1,-1],
+[-1,-1,-1,-1,-1],
+[-1,-1,-1,-1,-1],
+[-1,-1,-1,-1,-1],
+[-1,-1,-1,-1,-1]]).
+
+emptyBoard6x6([[-1,-1,-1,-1,-1,-1],
+[-1,-1,-1,-1,-1,-1],
+[-1,-1,-1,-1,-1,-1],
+[-1,-1,-1,-1,-1,-1],
+[-1,-1,-1,-1,-1,-1],
+[-1,-1,-1,-1,-1,-1]]).
+
+emptyBoard7x7([[-1,-1,-1,-1,-1,-1,-1],
+[-1,-1,-1,-1,-1,-1,-1],
+[-1,-1,-1,-1,-1,-1,-1],
+[-1,-1,-1,-1,-1,-1,-1],
+[-1,-1,-1,-1,-1,-1,-1],
+[-1,-1,-1,-1,-1,-1,-1],
+[-1,-1,-1,-1,-1,-1,-1]]).
 
 testBoard([[1,0,0,0],
 [0,3,0,0],
 [0,0,0,0],
 [0,4,0,0]]).
 
+testLineRestrictions41([-1,2,-1,1]).
+testLineRestrictions42([-1,-1,3,-1]).
+testLineRestrictions43([2,-1,3,-1]).
+testLineRestrictions44([-1,1,-1,-1]).
+
 testLineRestrictions1([-1,2,-1,1,-1]).
 testLineRestrictions2([1,-1,1,-1,3]).
 testLineRestrictions3([2,-1,3,-1,3]).
 testLineRestrictions4([-1,3,-1,1,-1]).
+
+testLineRestrictions51([4,-1,-1,-1,1]).
+testLineRestrictions52([-1,-1,-1,1,2]).
+testLineRestrictions53([-1,1,4,1,-1]).
+testLineRestrictions54([-1,2,1,-1,-1]).
+
+testLineRestrictions61([1,3,1,-1,-1,-1]).
+testLineRestrictions62([-1,2,-1,1,-1,-1]).
+testLineRestrictions63([4,-1,3,-1,-1,1]).
+testLineRestrictions64([-1,-1,-1,3,4,-1]).
+
+testLineRestrictions71([3,1,-1,-1,4,2,4]).
+testLineRestrictions72([-1,2,-1,-1,3,3,-1]).
+testLineRestrictions73([-1,2, 5,4,-1,4,-1]).
+testLineRestrictions74([1,-1,-1,1,-1, 2,4]).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -63,7 +104,7 @@ print('                 Easy as ABC').
 
 start:-
         
-         emptyBoard(Board),
+         emptyBoard5x5(Board),
          startmenu(Board).
 
 startmenu(Board) :-
@@ -212,16 +253,19 @@ constrainSides(_, _, Size2, Size):-
 constrainSides(Rest1, Rest2, I, Size):-
         element(I, Rest1, Elem1),
         element(I, Rest2, Elem2),
-        
-        Div1 is round(Size/2+1),
-        Div is round(Size/2),
-        nvalue(Div1, Rest1),
-        nvalue(Div1, Rest2),                            %%nvalue com erros em boards impares
-        count(0, Rest1, #=, Div),
-        count(0, Rest2, #=, Div),
-        Elem1 #\= Elem2 #/\ ((Elem1 #= 0 #/\ Elem2 #\= 0) #\/ (Elem1 #\= 0 #/\ Elem2 #= 0)),
+        Div is round(Size/2+1),
+        count(0, Rest1, #>, 1),                                         %%Check limits validity in Nxn
+        count(0, Rest2, #>, 1),
+        Elem1 #\= Elem2 #\/ ((Elem1 #= 0 #/\ Elem2 #\= 0) #\/ (Elem1 #\= 0 #/\ Elem2 #= 0)),
         I2 is I+1,
         constrainSides(Rest1, Rest2, I2, Size).
+
+defineEachValueLimits(_, Size, Size).
+
+defineEachValueLimits(Rest, I, Size):-
+        count(I, Rest, #<, 3),
+        I2 is I+1,
+        defineEachValueLimits(Rest, I2, Size).
         
 
 topBotVerification(Restrictions, Size):-
@@ -229,6 +273,8 @@ topBotVerification(Restrictions, Size):-
         PosI is Size*2+1,
         PosF is Size*3,
         getRowByPos(Restrictions,  PosI, PosF, [], RestriBot),
+        defineEachValueLimits(RestriTop, 1, Size),
+        defineEachValueLimits(RestriBot, 1, Size),
         constrainSides(RestriTop, RestriBot, 1, Size).
 
 leftRightVerification(Restrictions, Size):-
@@ -238,12 +284,135 @@ leftRightVerification(Restrictions, Size):-
         PosIL is Size*3+1,
         PosFL is Size*4,
         getRowByPos(Restrictions, PosIL, PosFL, [], RestriLeft),
+        defineEachValueLimits(RestriRight, 1, Size),
+        defineEachValueLimits(RestriLeft, 1, Size),
         constrainSides(RestriRight, RestriLeft, 1, Size).        
 
 oppositeSideVerification(Restrictions, Size):-
         topBotVerification(Restrictions, Size),
         leftRightVerification(Restrictions, Size).
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%  Special Cases Verification such as the restrictions that create corners, 1 cannot have more of each element than the other || Further information in THE notebook
+
+getTopRestriction(Restrictions, Size, RestriTop):-
+         getRowByPos(Restrictions, 1, Size, [], RestriTop).
+
+getRightRestriction(Restrictions, Size, RestriRight):-
+        PosIR is Size+1,
+        PosFR is Size*2,
+        getRowByPos(Restrictions, PosIR, PosFR, [], RestriRight).
+
+getBottomRestriction(Restrictions, Size, RestriBot):-
+        PosI is Size*2+1,
+        PosF is Size*3,
+        getRowByPos(Restrictions,  PosI, PosF, [], RestriBot).
+
+getLeftRestriction(Restrictions, Size, RestriLeft):-
+        PosIL is Size*3+1,
+        PosFL is Size*4,
+        getRowByPos(Restrictions, PosIL, PosFL, [], RestriLeft).    
+
+constrainCornersLT(_, _, Size, Size).
+
+constrainCornersLT(RestriTop, RestriLeft, I, Size):-
+        element(1, RestriTop, P1),
+        element(2, RestriTop, P2),
+        element(2, RestriLeft, P3),
+        
+        ((P1 #= I #/\ P3 #= I) #\/ P1 #\= I) #\/ ((P2 #= I #/\ P3 #= I) #\/ P2 #\= I),
+        I2 is I+1,
+        constrainCornersLT(RestriTop, RestriLeft, I2, Size).
+
+constrainCornersLB(_, _, Size, Size).
+
+constrainCornersLB(RestriBot, RestriLeft, I, Size):-
+        Pos1 is Size-1,
+        element(Size, RestriLeft, P1),
+        element(Pos1, RestriLeft, P2),
+        element(2, RestriBot, P3),
+        ((P1 #= I #/\ P3 #= I) #\/ P1 #\= I) #\/ ((P2 #= I #/\ P3 #= I) #\/ P2 #\= I),
+        I2 is I+1,
+        constrainCornersLB(RestriBot, RestriLeft, I2, Size).
+
+constrainCornersBL(_, _, Size, Size).
+
+constrainCornersBL(RestriBot, RestriLeft, I, Size):-
+        Pos1 is Size-1,
+        element(1, RestriBot, P1),
+        element(2, RestriBot, P2),
+        element(Pos1, RestriLeft, P3),
+        ((P1 #= I #/\ P3 #= I) #\/ P1 #\= I) #\/ ((P2 #= I #/\ P3 #= I) #\/ P2 #\= I),
+        I2 is I+1,
+        constrainCornersBL(RestriBot, RestriLeft, I2, Size).
+
+constrainCornersBR(_, _, Size, Size).
+
+constrainCornersBR(RestriBot, RestriRight, I, Size):-
+        Pos1 is Size,
+        Pos2 is Size-1,
+        element(Pos1, RestriBot, P1),
+        element(Pos2, RestriBot, P2),
+        element(Pos2, RestriRight, P3),
+        ((P1 #= I #/\ P3 #= I) #\/ P1 #\= I) #\/ ((P2 #= I #/\ P3 #= I) #\/ P2 #\= I),
+        I2 is I+1,
+        constrainCornersBR(RestriBot, RestriRight, I2, Size).
+
+constrainCornersTR(_, _, Size, Size).
+
+constrainCornersTR(RestriTop, RestriRight, I, Size):-
+        Pos1 is Size-1,
+        element(Size, RestriRight, P1),
+        element(Pos1, RestriRight, P2),
+        element(2, RestriTop, P3),
+        ((P1 #= I #/\ P3 #= I) #\/ P1 #\= I) #\/ ((P2 #= I #/\ P3 #= I) #\/ P2 #\= I),
+        I2 is I+1,
+        constrainCornersTR(RestriTop, RestriRight, I2, Size).
+
+constrainCornersTR(_, _, Size, Size).
+
+constrainCornersTR(RestriTop, RestriRight, I, Size):-
+        Pos1 is Size-1,
+        element(Size, RestriTop, P1),
+        element(Pos1, RestriTop, P2),
+        element(2, RestriRight, P3),
+        ((P1 #= I #/\ P3 #= I) #\/ P1 #\= I) #\/ ((P2 #= I #/\ P3 #= I) #\/ P2 #\= I),
+        I2 is I+1,
+        constrainCornersTR(RestriTop, RestriRight, I2, Size).
+
+constrainCornersRT(_, _, Size, Size).
+
+constrainCornersRT(RestriTop, RestriRight, I, Size):-
+        Pos1 is Size-1,
+        element(1, RestriRight, P1),
+        element(2, RestriRight, P2),
+        element(Pos1, RestriTop, P3),
+        ((P1 #= I #/\ P3 #= I) #\/ P1 #\= I) #\/ ((P2 #= I #/\ P3 #= I) #\/ P2 #\= I),
+        I2 is I+1,
+        constrainCornersRT(RestriTop, RestriRight, I2, Size).
+
+
+constrainCorneredRestrictions(Restrictions, Size):-
+        getTopRestriction(Restrictions, Size, RestriTop),
+        getLeftRestriction(Restrictions, Size, RestriLeft),
+        getRightRestriction(Restrictions, Size, RestriRight),
+        getBottomRestriction(Restrictions, Size, RestriBot),
+        constrainCornersLT(RestriTop, RestriLeft, 1, Size),
+        constrainCornersLT(RestriLeft, RestriTop, 1, Size),
+        
+        constrainCornersLB(RestriBot, RestriLeft, 1, Size),
+        constrainCornersBL(RestriBot, RestriLeft, 1, Size),
+        
+        constrainCornersBR(RestriBot, RestriRight, 1, Size),        
+        constrainCornersBR(RestriRight, RestriBot, 1, Size),
+        
+        constrainCornersTR(RestriTop, RestriRight, 1, Size),
+        constrainCornersRT(RestriTop, RestriRight, 1, Size).
+
+        
+       
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -276,15 +445,54 @@ placeRandomValueInEachRow(Restrictions, C, I, Size):-
         I2 is I+1,
         placeRandomValueInEachRow(Restrictions, C2, I2, Size).
 
+placeRandomValueInEachRow(Restrictions, Size):-
+        getBottomRestriction(Restrictions, Size, Bot),
+        getTopRestriction(Restrictions, Size, Top),
+        
+        Limit1 is Size+1,
+        
+        random(1, Limit1, PosT),      
+        random(1, Size, ValueT),
+       
+
+        random(1, Limit1, PosB),      
+        random(1, Size, ValueB),
+        PosT #\= PosB,
+        
+        element(PosT, Top, ValueT),
+        element(PosB, Bot, ValueB).
+
+placeRandomValueInEachRow(Restrictions, Size):-
+        Limit1 is Size+1,
+        
+        random(1, Limit1, PosT),      
+        random(1, Size, _),
+       
+
+        random(1, Limit1, PosB),      
+        random(1, Size, _),
+        PosT #= PosB,
+        placeRandomValueInEachRow(Restrictions, Size).
+        
+constrainRepeatValue(_, Size, Size).
+
+constrainRepeatValue(Restrictions, I, Size):-
+        Value is 5+1,
+        I2 is I+1,
+        count(I, Restrictions, #<, Value),
+        constrainRepeatValue(Restrictions, I2, Size).
+        
+
 generateRandomPlainRestrictions(Restrictions, Size):-
         N is Size * Size,
         length(Restrictions, N),
         S is Size-1,
         domain(Restrictions, 0,  S),
-        
-        placeRandomValueInEachRow(Restrictions, 0, 1, Size),
+        placeRandomValueInEachRow(Restrictions, Size),
         oppositeSideVerification(Restrictions, Size),
-        cornersVerification(Restrictions, Size),    
+        constrainCorneredRestrictions(Restrictions, Size),
+        constrainRepeatValue(Restrictions, 1, Size),
+        %%cornersVerification(Restrictions, Size),
         labeling([], Restrictions).          
         
 
@@ -307,11 +515,15 @@ startDynamic(L):-
         setrand(Secs),
         L1 is L+1,
         
-       /* testLineRestrictions1(R1),
-        testLineRestrictions2(R2),
-        testLineRestrictions3(R3),
-        testLineRestrictions4(R4),*/
+      /*  testLineRestrictions51(R1),
+        testLineRestrictions52(R2),
+        testLineRestrictions53(R3),
+        testLineRestrictions54(R4),
         
+        emptyBoard5x5(A),nl,
+        
+        printScenario(A, L1, R1, R2, R3, R4),nl,
+       */
         generateRandomPlainRestrictions(Restrictions, L),        
         createBoard(Restrictions, [], [], RestList, L1, 1, 1),
        
@@ -319,9 +531,12 @@ startDynamic(L):-
         getRow(Rest, Rest1, R2),
         getRow(Rest1, Rest2, R3),
         getRow(Rest2, _, R4),
-        emptyBoard(A),nl,
+        
+        emptyBoard5x5(A),nl,
+        
         printScenario(A, L1, R1, R2, R3, R4),nl,
         
+
         dynamicGame(Board, R1,R2,R3,R4, L),      
                 
         createBoard(Board, [], [], Scene, L1, 1, 1),
@@ -329,6 +544,21 @@ startDynamic(L):-
         printScenario(Scene, L1, R1, R2, R3, R4),
         startmenu(Scene).
         
+
+dynamicGame(Board, R1, R2, R3, R4, L):-                                 %%Verify board lentgh verification because its failling.
+        L1 is L*L,
+        length(Board, L1), 
+        DL is L-1,
+        domain(Board, 0, DL),
+        L2 is L+1, 
+        createBoard(Board, [], [], Scene, L2, 1, 1),
+        constrainRows(Scene, L),
+        constrainColumns(Scene, L),        
+        constrainSides(Scene, R1, R2, R3, R4, L), 
+        append(Scene, BoardOut),       
+        labeling([], BoardOut).   
+
+
   dynamicGame(Board, R1, R2, R3, R4, L):-                                 %%Verify board lentgh verification because its failling.
         L1 is L*L,
         length(Board, L1), 
@@ -345,18 +575,7 @@ startDynamic(L):-
         write('There is no solution for the generated board. Trying again... '),nl,nl,
         startDynamic(L).
       
-dynamicGame(Board, R1, R2, R3, R4, L):-                                 %%Verify board lentgh verification because its failling.
-        L1 is L*L,
-        length(Board, L1), 
-        DL is L-1,
-        domain(Board, 0, DL),
-        L2 is L+1, 
-        createBoard(Board, [], [], Scene, L2, 1, 1),
-        constrainRows(Scene, L),
-        constrainColumns(Scene, L),        
-        constrainSides(Scene, R1, R2, R3, R4, L), 
-        append(Scene, BoardOut),       
-        labeling([], BoardOut).   
+
 
 
 
@@ -453,6 +672,7 @@ constrain(Row1, Row2, R1, I, Size):-
         element(I, Row1, ER1),
         element(I, Row2, ER2),
         element(I, R1, R),
-        (ER1 #= 0 #/\ ER2 #= R) #\/ ER1 #= R ,
-        
-        constrain(Row1, Row2, R1, I2, Size).            
+        (ER1 #= 0 #/\ ER2 #= R) #\/ ER1 #= R ,        
+        constrain(Row1, Row2, R1, I2, Size). 
+
+           
